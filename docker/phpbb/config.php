58a137217715dd176164dbde01fdd0f5c7bfa182
tengfei2015 <xitengfei@aragoncs.com>
2017-11-03 17:09:11 +0800

    integrate with docker-compose

--- docker/phpbb/config.php
+++ docker/phpbb/config.php
@@ -2,7 +2,7 @@
 // phpBB 3.2.x auto-generated configuration file
 // Do not change anything in this file!
 $dbms = 'phpbb\\db\\driver\\mysqli';
-$dbhost = '172.18.0.2';
+$dbhost = 'db';
 $dbport = '3306';
 $dbname = 'phpbb';
 $dbuser = 'root';
